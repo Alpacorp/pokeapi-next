e6a96a2014ccc95057e1a3ca49cc68b30ae0e62a
Alpacorp <alejandro.palacios88@gmail.com>
2022-09-05 22:20:59 -0500

    add page to name pokemon search

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -1,6 +1,15 @@
 import { Layout } from "../../components/layouts";
+import { Pokemon } from "../../interfaces";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { pokeApi } from "../../api";
 
-export const PokemonByNamePage = () => {
+interface Props {
+  pokemon: Pokemon;
+}
+
+export const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
+  console.log("pokemon", pokemon);
+
   return (
     <Layout>
       <div>
@@ -8,4 +17,29 @@ export const PokemonByNamePage = () => {
       </div>
     </Layout>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pokemonsNames151 = [...Array(151)].map(
+    (value, index) => `${index + 1}`
+  );
+
+  return {
+    paths: pokemonsNames151.map((id) => ({
+      params: { id },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { name } = params as { name: string };
+
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${name}`);
+
+  return {
+    props: {
+      pokemon: data,
+    },
+  };
 };
